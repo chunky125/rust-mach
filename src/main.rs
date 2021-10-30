bf264dbf016baf8809cb296a11cb50739f4a5f76
Chris Plant <chris@monkeyircd.org>
2021-10-30 10:47:50 +0100

    Merge multiboot2/tag.rs into multiboot.rs to simplify tag structure.

--- src/main.rs
+++ src/main.rs
@@ -25,13 +25,43 @@ fn boot_entry(mbmagic: u64, mbinfoaddr: u64, _bootarg3: u64, _bootarg4: u64) {
     // Say Hello!
     mu.send_string("\x1b[2JRust Mach OS, initialising\r\n");
 
-    // Create a MB2 Information structure - this is a singleton
+    // Create a MB2 Information structure - this is a singleton - how to implement it!
     use multiboot2::Multiboot2;
     let mbinfo: Multiboot2 = Multiboot2::create(mbinfoaddr, mbmagic);
 
     if mbinfo.valid() == true {
+
         mu.send_string("Multiboot2 Info is Valid\r\n");
+
+        if mbinfo.tags.is_some() {
+            for mb2tag in mbinfo.tags.unwrap() {
+
+                use multiboot2::Tag;
+                
+                match mb2tag {
+
+                    Tag::CmdLine {value, .. } => {
+                        mu.send_string("Found Command Line MB2 Tag\r\n");
+                    }
+
+                    Tag::EFI64SystemTable{addr, .. } => {
+                        use heapless::String;
+                        use core::fmt::Write;
+
+                        let mut systab_text : String::<128> = String::<128>::new();
+
+                        writeln!(systab_text,"EFI System table is at {:x}\r\n", addr);
+
+                        mu.send_string(&systab_text);
+                    }
+
+                    _ => {
+                    }
+                }
+            }
+        }
     }
+
 
     // Start the Kernel itself
     loop {
--- src/multiboot2.rs
+++ src/multiboot2.rs
@@ -5,17 +5,13 @@
 //
 // FIXME: This should be a singleton
 
-mod tag;
-
-use tag::Tag;
-use heapless::Vec;
-use typenum::{U10};
+use heapless::{Vec,String};
 
 pub struct Multiboot2 {
     addr: u64,   // Pointer to rawdata in memory
     magic: u64,  // Magic number
     valid: bool, // Have we checked that this data is valid
-    tags : Option<Vec<Tag,U10>>, // Multiboot tags
+    pub tags : Option<Vec<Tag,10>>, // Multiboot tags
 }
 
 impl Multiboot2 {
@@ -51,24 +47,24 @@ impl Multiboot2 {
         // Do some processing on it
         let mb2infosizeptr: *const u32 = init_addr as *const u32;
         let mut mb2currtagptr: u64 = init_addr + 8;
-        let mut new_tags = Vec::<Tag,U10>::new();
+        let mut new_tags = Vec::<Tag,10>::new();
 
         // We have to do some unsafe memory accesses onto the data provided
         // by the bootloader here, then we copy to the various places
         unsafe {
             while mb2currtagptr < init_addr + *mb2infosizeptr as u64 {
                 
-                let new_tag : Option<Tag> = Tag::from_addr(mb2currtagptr);
+                let new_tag : Tag = Tag::from_addr(mb2currtagptr);
 
-                if new_tag.is_some() {
-                    let unwrapped_tag : Tag = new_tag.unwrap();
+                mb2currtagptr = mb2currtagptr + new_tag.size_align(8) as u64;
 
-                    mb2currtagptr = mb2currtagptr + (unwrapped_tag.size() as u64);
-                    new_tags.push (unwrapped_tag);
+                let push_result = new_tags.push (new_tag);
+
+                if push_result.is_err() {
+                    panic!();
                 } else {
                     // Log an error
                 }
-
             }
         }
 
@@ -85,3 +81,223 @@ impl Multiboot2 {
         return self.valid;
     }
 }
+
+/// Structure for Elf Section Table Entry
+pub struct ELFSectionEntry {
+    section_name_addr : u32,
+    section_type : u32,
+    section_flags : u64,
+    section_addr : u64,
+    section_size : u64,
+    section_link : u32,
+    section_info : u32,
+    section_addr_align : u64,
+    section_entsize : u64,
+}
+
+/// Structure for memory map entry
+pub struct MMapEntry {
+    base_addr : u64,
+    length : u64,
+    region_type : u32,
+}
+
+/// Enum for the tag type
+pub enum Tag {
+
+    End { 
+        tag_size : u32,
+    },
+    CmdLine { 
+        tag_size : u32,
+        value : String<256>,
+    },
+    BootLoaderName {
+        tag_size : u32,
+        name : String<64>,
+    },
+    Module {
+        tag_size : u32,
+        mod_start: u32,
+        mod_end: u32,
+        mod_name: String<64>,
+    },
+    /* BootDev is not implemented */
+    Mmap {
+        tag_size : u32,
+        table : Vec<MMapEntry,10>,
+    },
+    /* VBE and Framebuffer not implemented
+     * ELFSections tag is not properly implemented in 
+     * GRUB, instead we use ELF64Sections
+     * APM, EFI 32 not implemented */
+    EFI64SystemTable {
+        tag_size : u32,
+        addr : u64,
+    },
+    /* SMBIOS, ACPIOLD, ACPI, Network, EFI Mmap not implemented */
+    EFIBootServicesRunning {
+        tag_size : u32,
+    },
+    /* EFI32IH not implemented */
+    //EFI64ImageHandler {
+    //    addr : u64
+    //}, 
+    LoadBaseAddress {
+        tag_size : u32,
+        base_address: u32,
+    },
+    ELF64Sections {
+        tag_size : u32,
+        table : Vec<ELFSectionEntry, 10>,
+    },
+    Unknown {
+        tag_size : u32,
+    },
+}
+
+impl Tag {
+    pub fn from_addr(tag_base_addr : u64) -> Tag {
+        unsafe {
+            let new_size : u32 = *((tag_base_addr + 4) as *const u32);
+            let new_type_no : u32 = *(tag_base_addr as *const u32);
+
+            match new_type_no {
+
+                // End 
+                0 => {
+                    Tag::End { 
+                        tag_size : new_size 
+                    }
+                }
+
+                // Command Line
+                1 => {
+                    Tag::CmdLine {
+                        tag_size : new_size,
+                        value : String::from("CommandLine"),
+                    }
+                }
+
+                // Bootloader Name
+                2 => {
+                    Tag::BootLoaderName {
+                        tag_size : new_size,
+                        name : String::from("BootLoaderName"),
+                    }
+                }
+
+                // Module
+                3 => {
+                    Tag::Module {
+                        tag_size : new_size,
+                        mod_start : *((tag_base_addr + 8) as *const u32),
+                        mod_end : *((tag_base_addr + 12) as *const u32),
+                        mod_name : String::from("Module"),
+                    }
+                }
+
+                // Memory Map
+                6 => {
+                    let mmap_entry_size = *((tag_base_addr + 8) as *const u32);
+                    let mmap_entry_count = *((tag_base_addr + 12) as *const u32);
+                    let mut mmap_table = Vec::<MMapEntry,10>::new();
+
+                    let mut mmap_curr_addr = tag_base_addr + 16;
+
+                    for i in 0..mmap_entry_count {
+                        let new_entry = MMapEntry { 
+                            base_addr : *(mmap_curr_addr as *const u64),
+                            length : *((mmap_curr_addr + 8) as *const u64),
+                            region_type : *((mmap_curr_addr + 16) as *const u32),
+                        };
+                        
+                        let push_result = mmap_table.push(new_entry);
+
+                        if push_result.is_err() {
+                            panic!();
+                        }
+
+                        mmap_curr_addr += mmap_entry_size as u64;
+                    }
+                        
+                    Tag::Mmap {
+                        tag_size : new_size,
+                        table : mmap_table,
+                    }
+                }
+        
+                
+                // EFI64 System Table Pointer
+                12 => {
+                    Tag::EFI64SystemTable { 
+                        tag_size : new_size,
+                        addr : *((tag_base_addr + 8) as *const u64) 
+                    }
+                }  
+
+                // EFI Boot Services Running
+                18 => {
+                    Tag::EFIBootServicesRunning {
+                        tag_size : new_size,
+                    }
+                }
+
+                // Load Base Address
+                21 => {
+                    Tag::LoadBaseAddress {
+                        tag_size : new_size,
+                        base_address : *((tag_base_addr + 8) as *const u32) 
+                    }
+                }
+                
+                // ELF64 Headers
+                22 => {
+                    let new_table = Vec::<ELFSectionEntry,10>::new();
+
+                    Tag::ELF64Sections {
+                        tag_size : new_size,
+                        table : new_table,
+                    }
+                }
+                
+                // Something has gone wrong
+                _ => {
+                    
+                    Tag::Unknown {
+                        tag_size : new_size,
+                    }
+                }
+            }   
+        }
+    }
+
+    pub fn size(&self) -> u32 {
+
+        match self {
+            Tag::End { tag_size } |
+            Tag::CmdLine { tag_size, .. } |
+            Tag::BootLoaderName { tag_size, .. } |
+            Tag::Module { tag_size, .. } |
+            Tag::Mmap { tag_size, .. } | 
+            Tag::EFI64SystemTable { tag_size, .. } |
+            Tag::EFIBootServicesRunning { tag_size, .. } |
+            Tag::LoadBaseAddress { tag_size, .. } | 
+            Tag::ELF64Sections { tag_size, .. } |           
+            Tag::Unknown { tag_size, .. } => {
+                *tag_size
+            }
+            
+            _ => {
+                0
+            }
+        }
+    }
+
+    pub fn size_align(&self, align : u32) -> u32 {
+        
+        (self.size() + (align - 1)) & !(align-1)
+    }
+
+}
+
